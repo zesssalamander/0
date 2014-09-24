135aacd9a54eb70c72422037d7d102821082163d
unknown <zesssalamander27@hotmail.com>
2014-09-23 19:44:18 -0500

    Base del proyecto

---
+++ trabajo0/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Usuario
+ * Date: 23/09/2014
+ * Time: 07:36 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace trabajo0
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
